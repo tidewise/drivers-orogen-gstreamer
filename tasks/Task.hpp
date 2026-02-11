80546528a52b22b0d26749a55089dec67c0f37f3
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2026-02-11 13:30:53 -0300

    fix: add missing include

--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -9,6 +9,7 @@
 #include "gstreamer/TaskBase.hpp"
 #include "iodrivers_base/RawPacket.hpp"
 
+#include <cstdint>
 #include <gst/app/gstappsink.h>
 #include <gst/app/gstappsrc.h>
 #include <gst/video/video-info.h>
